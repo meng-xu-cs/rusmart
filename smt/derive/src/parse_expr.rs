2052204bda422048c52a61b7b162e096dd9e31f3
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-10-04 18:10:34 -0400

    x

--- smt/derive/src/parse_expr.rs
+++ smt/derive/src/parse_expr.rs
@@ -1,6 +1,10 @@
-use crate::parse_ctxt::{FuncName, TypeName};
+use syn::{Local, LocalInit, Stmt};
+
+use std::collections::BTreeMap;
+
+use crate::parse_ctxt::{bail_if_exists, bail_if_missing, bail_on, FuncName, TypeName, VarName};
 use crate::parse_func::FuncSig;
-use crate::parse_type::TypeDef;
+use crate::parse_type::{TypeDef, TypeUse};
 
 /// A context suitable for expression analysis
 pub trait CtxtForExpr {
@@ -12,4 +16,225 @@ pub trait CtxtForExpr {
 
     /// Retrieve the function signature for spec
     fn get_spec_sig(&self, name: &FuncName) -> Option<&FuncSig>;
+}
+
+/// Intrinsic procedure
+pub enum Intrinsic {
+    /// `Boolean::new`
+    BoolVal(bool),
+    /// `Boolean::not`
+    Not(Expr),
+    /// `Boolean::and`
+    And(Expr, Expr),
+    /// `Boolean::or`
+    Or(Expr, Expr),
+    /// `Boolean::xor`
+    Xor(Expr, Expr),
+    /// `Integer::new`
+    IntVal(i128),
+    /// `Integer::eq`
+    IntEq(Expr, Expr),
+    /// `Integer::ne`
+    IntNe(Expr, Expr),
+    /// `Integer::lt`
+    IntLt(Expr, Expr),
+    /// `Integer::le`
+    IntLe(Expr, Expr),
+    /// `Integer::ge`
+    IntGe(Expr, Expr),
+    /// `Integer::gt`
+    IntGt(Expr, Expr),
+    /// `Integer::add`
+    IntAdd(Expr, Expr),
+    /// `Integer::sub`
+    IntSub(Expr, Expr),
+    /// `Integer::mul`
+    IntMul(Expr, Expr),
+    /// `Integer::div`
+    IntDiv(Expr, Expr),
+    /// `Integer::rem`
+    IntRem(Expr, Expr),
+    /// `Rational::new`
+    NumVal(i128),
+    /// `Rational::eq`
+    NumEq(Expr, Expr),
+    /// `Rational::ne`
+    NumNe(Expr, Expr),
+    /// `Rational::lt`
+    NumLt(Expr, Expr),
+    /// `Rational::le`
+    NumLe(Expr, Expr),
+    /// `Rational::ge`
+    NumGe(Expr, Expr),
+    /// `Rational::gt`
+    NumGt(Expr, Expr),
+    /// `Rational::add`
+    NumAdd(Expr, Expr),
+    /// `Rational::sub`
+    NumSub(Expr, Expr),
+    /// `Rational::mul`
+    NumMul(Expr, Expr),
+    /// `Rational::div`
+    NumDiv(Expr, Expr),
+    /// `Text::new`
+    StrVal(String),
+    /// `Text::eq`
+    StrEq(Expr, Expr),
+    /// `Text::ne`
+    StrNe(Expr, Expr),
+    /// `Text::lt`
+    StrLt(Expr, Expr),
+    /// `Text::le`
+    StrLe(Expr, Expr),
+    /// `Seq::empty`
+    SeqEmpty,
+    /// `Seq::append`
+    SeqAppend(Expr, Expr),
+    /// `Seq::length`
+    SeqLength(Expr),
+    /// `Seq::contains`
+    SeqContains(Expr, Expr),
+    /// `Seq::at_unchecked`
+    SeqAt(Expr, Expr),
+    /// `Set::empty`
+    SetEmpty,
+    /// `Set::insert`
+    SetInsert(Expr, Expr),
+    /// `Set::length`
+    SetLength(Expr),
+    /// `Set::contains`
+    SetContains(Expr, Expr),
+    /// `Map::empty`
+    MapEmpty,
+    /// `Map::put_unchecked`
+    MapPut(Expr, Expr, Expr),
+    /// `Map::get_unchecked`
+    MapGet(Expr, Expr),
+    /// `Map::length`
+    MapLength(Expr),
+    /// `Map::contains_key`
+    MapContainsKey(Expr, Expr),
+    /// `Error::fresh`
+    ErrFresh,
+    /// `Error::merge`
+    ErrMerge(Expr, Expr),
+}
+
+/// Phi node, guarded by condition
+pub struct PhiNode {
+    cond: Expr,
+    body: Expr,
+}
+
+/// Operations
+pub enum Op {
+    /// `<var>`
+    Var(VarName),
+    /// `&<expr>`
+    Ref(Expr),
+    /// `if (<c1>) { <v1> } else if (<c2>) { <v2> } ... else { <default> }`
+    Phi { nodes: Vec<PhiNode>, default: Expr },
+    /// `<class>::<method>(<a1>, <a2>, ...)`
+    Intrinsic(Intrinsic),
+    /// `<function>(<a1>, <a2>, ...)`
+    Procedure { name: FuncName, args: Vec<Expr> },
+}
+
+/// Instructions (operations with type)
+pub struct Inst {
+    op: Box<Op>,
+    ty: TypeUse,
+}
+
+/// Expressions
+pub enum Expr {
+    /// a single instruction
+    Unit(Inst),
+    /// `{ let <v1> = ...; let <v2> = ...; ...; <op>(<v1>, <v2>, ...) }`
+    Block {
+        lets: Vec<(VarName, Expr)>,
+        body: Inst,
+    },
+}
+
+impl Expr {
+    /// Retrieve the type of an expression
+    pub fn ty(&self) -> &TypeUse {
+        let i = match self {
+            Self::Unit(inst) => inst,
+            Self::Block { lets: _, body } => body,
+        };
+        &i.ty
+    }
+}
+
+/// Marks whether this expression is for impl or spec
+pub enum Kind {
+    /// actual implementation
+    Impl,
+    /// formal specification
+    Spec,
+}
+
+/// A helper for expression parsing
+struct ExprParseCtxt<'a, 'b> {
+    kind: Kind,
+    vars: &'a BTreeMap<VarName, &'b TypeUse>,
+    bindings: Vec<(VarName, Expr)>,
+    new_vars: BTreeMap<VarName, TypeUse>,
+}
+
+impl<'a, 'b> ExprParseCtxt<'a, 'b> {
+    /// For parsing in impl context
+    fn for_impl(vars: &'a BTreeMap<VarName, &'b TypeUse>) -> Self {
+        Self {
+            kind: Kind::Impl,
+            vars,
+            bindings: vec![],
+            new_vars: BTreeMap::new(),
+        }
+    }
+
+    /// For parsing in spec context
+    fn for_spec(vars: &'a BTreeMap<VarName, &'b TypeUse>) -> Self {
+        Self {
+            kind: Kind::Spec,
+            vars,
+            bindings: vec![],
+            new_vars: BTreeMap::new(),
+        }
+    }
+
+    /// Consume the stream of statements
+    fn convert_stmts(&mut self, stmts: &[Stmt]) -> Result<Expr> {
+        for stmt in stmts {
+            match stmt {
+                Stmt::Local(binding) => {
+                    // this is a let-binding
+                    let Local {
+                        attrs: _,
+                        let_token: _,
+                        pat,
+                        init,
+                        semi_token: _,
+                    } = binding;
+
+                    let name = VarName::from_pat(pat)?;
+                    let body = bail_if_missing!(init, binding, "expect initializer");
+                    let LocalInit {
+                        eq_token: _,
+                        expr,
+                        diverge,
+                    } = body;
+                    bail_if_exists!(diverge);
+                }
+                Stmt::Expr(expr, semi_token) => {
+                    // expecting a unit expression
+                    bail_if_exists!(semi_token);
+                }
+                Stmt::Item(_) | Stmt::Macro(_) => bail_on!(stmt, "unexpected item"),
+            }
+        }
+        todo!()
+    }
 }
