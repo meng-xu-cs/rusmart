6bb3fa518d49523976b601514e84a06a4f59a989
Meng Xu <meng.xu.cs@uwaterloo.ca>
2023-10-13 01:05:45 -0300

    x

--- smt/derive/src/parser/generics.rs
+++ smt/derive/src/parser/generics.rs
@@ -14,6 +14,7 @@ use crate::parser::name::{ReservedIdent, TypeParamName};
 use crate::parser::path::PathUtil;
 
 /// Reserved trait
+#[allow(clippy::upper_case_acronyms)]
 pub enum SysTrait {
     SMT,
 }
