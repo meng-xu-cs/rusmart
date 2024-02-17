ede73c49b70fac6d6747130ad1acdb25ea474bd3
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-17 19:28:09 -0400

    x

--- smt/testing/tests/model/parser/attr_ok.rs
+++ smt/testing/tests/model/parser/attr_ok.rs
@@ -1,4 +1,4 @@
-use rusmart_smt_remark::smt_type;
+use rusmart_smt_remark::{smt_impl, smt_spec, smt_type};
 use rusmart_smt_stdlib::dt::Boolean;
 
 #[smt_type]
@@ -9,4 +9,34 @@ enum E {
 #[smt_type]
 struct S {
     f1: Boolean,
+}
+
+#[smt_impl]
+fn f1() -> Boolean {
+    Boolean::from(false)
+}
+
+#[smt_spec]
+fn f2() -> Boolean {
+    Boolean::from(false)
+}
+
+#[smt_impl(method = m3)]
+fn f3(s: S) -> Boolean {
+    s.f1.not()
+}
+
+#[smt_spec(method = m4)]
+fn f4(s: S) -> Boolean {
+    s.f1.not()
+}
+
+#[smt_impl(specs = f6)]
+fn f5(s: S) -> Boolean {
+    f3(s)
+}
+
+#[smt_spec(impls = f5)]
+fn f6(s: S) -> Boolean {
+    f4(s)
 }
