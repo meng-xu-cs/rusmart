e8df04aef4a8f8cc2c0a8a95347faa083d0450e8
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-18 12:21:59 -0400

    x

---
+++ smt/testing/tests/model/parser/invalid_spec_target.rs
@@ -0,0 +1,7 @@
+use rusmart_smt_remark::smt_impl;
+use rusmart_smt_stdlib::dt::Boolean;
+
+#[smt_impl(specs = does_non_exist)]
+fn foo() -> Boolean {
+    Boolean::from(false)
+}
--- smt/testing/tests/model/parser/mod.rs
+++ smt/testing/tests/model/parser/mod.rs
@@ -3,5 +3,7 @@ mod duplicated_axiom;
 mod duplicated_impl;
 mod duplicated_spec;
 mod duplicated_type;
+mod invalid_impl_target;
+mod invalid_spec_target;
 mod name_conflict_on_impl_and_spec;
 mod name_conflict_on_type_and_axiom;
