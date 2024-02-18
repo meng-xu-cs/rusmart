757961e59aab9ad1392eeca829ac3711af42a031
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-18 12:12:53 -0400

    x

--- smt/testing/tests/model/parser/mod.rs
+++ smt/testing/tests/model/parser/mod.rs
@@ -3,4 +3,5 @@ mod duplicated_axiom;
 mod duplicated_impl;
 mod duplicated_spec;
 mod duplicated_type;
-mod name_conflict_on_fn;
+mod name_conflict_on_impl_and_spec;
+mod name_conflict_on_type_and_axiom;
