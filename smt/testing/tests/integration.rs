a00f528e212380b2378b33ee8924e6462cf1623e
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-21 16:28:27 +0100

    x

--- smt/testing/tests/integration.rs
+++ smt/testing/tests/integration.rs
@@ -1,5 +1,6 @@
 mod model;
 
+use std::collections::BTreeSet;
 use std::path::Path;
 use std::{env, fs};
 
@@ -10,16 +11,74 @@ use rusmart_smt_derive::model;
 
 static ENV_UPDATE_BASELINE: &str = "UPBL";
 
+fn check_mod(path: &Path) -> datatest_stable::Result<()> {
+    // read the file
+    let mut modules = BTreeSet::new();
+    let content = fs::read_to_string(path)?;
+    for line in content.lines() {
+        let line = line.trim();
+        if line.is_empty() {
+            continue;
+        }
+        match line.strip_prefix("mod ").and_then(|e| e.strip_suffix(";")) {
+            None => {
+                return Err(anyhow!("invalid line: {}", line).into());
+            }
+            Some(base) => {
+                modules.insert(base.to_string());
+            }
+        }
+    }
+
+    // read the directory
+    let mut files = BTreeSet::new();
+    let path_dir = path.parent().expect("mod directory");
+    for entry in fs::read_dir(path_dir)? {
+        let entry = entry?;
+        let name = entry
+            .file_name()
+            .into_string()
+            .expect("ascii filename only");
+        if entry.file_type()?.is_dir() {
+            if !entry.path().join("mod.rs").is_file() {
+                return Err(anyhow!("no sub-module {}", name).into());
+            }
+            files.insert(name);
+        } else {
+            match name.strip_suffix(".rs") {
+                None | Some("mod") => continue,
+                Some(base) => {
+                    files.insert(base.to_string());
+                }
+            }
+        }
+    }
+
+    // cross-check
+    for name in &modules {
+        if !files.contains(name) {
+            return Err(anyhow!("mod {} without backing file", name).into());
+        }
+    }
+    for name in &files {
+        if !modules.contains(name) {
+            return Err(anyhow!("file {} without backing mod", name).into());
+        }
+    }
+
+    Ok(())
+}
+
 /// Generic test runner for all front-end test cases
 fn test_model(path: &Path) -> datatest_stable::Result<()> {
-    // skip tests
+    // mod.rs are tested differently
     let base = path
         .file_name()
         .expect("filename")
         .to_str()
         .expect("ascii-based filename");
     if base == "mod.rs" {
-        return Ok(());
+        return check_mod(path);
     }
 
     // load existing message
@@ -47,27 +106,24 @@ fn test_model(path: &Path) -> datatest_stable::Result<()> {
             }
         }
         (Ok(_), Some(exp)) => {
-            if !ok_hint {
-                return Err(anyhow!("test passed without the `_ok` suffix").into());
-            }
             if !update {
                 return Err(anyhow!("test passed while expecting failure\n{}", exp).into());
+            }
+            if !ok_hint {
+                return Err(anyhow!("test passed without the `_ok` suffix").into());
             }
             fs::remove_file(path_exp)?;
         }
         (Err(err), None) => {
-            if ok_hint {
-                return Err(anyhow!("test failed with the `_ok` suffix").into());
-            }
             if !update {
                 return Err(anyhow!("test failed while expecting success\n{}", err).into());
+            }
+            if ok_hint {
+                return Err(anyhow!("test failed with the `_ok` suffix").into());
             }
             fs::write(path_exp, err.to_string())?;
         }
         (Err(err), Some(exp)) => {
-            if ok_hint {
-                return Err(anyhow!("test failed with the `_ok` suffix").into());
-            }
             let msg = err.to_string();
             if exp != msg {
                 if !update {
@@ -76,9 +132,12 @@ fn test_model(path: &Path) -> datatest_stable::Result<()> {
                         exp,
                         msg
                     )
-                    .into());
+                        .into());
                 }
                 fs::write(path_exp, msg)?;
+            }
+            if ok_hint {
+                return Err(anyhow!("test failed with the `_ok` suffix").into());
             }
         }
     };
--- smt/testing/tests/model/parser/mod.rs
+++ smt/testing/tests/model/parser/mod.rs
@@ -1,6 +1,7 @@
 mod adt_decl_enum_discriminant;
 mod adt_enum_no_variants;
 mod adt_enum_ok;
+mod adt_enum_variant_record_no_fields;
 mod adt_enum_variant_tuple_no_slots;
 mod adt_struct_as_unit;
 mod adt_struct_no_fields;
@@ -14,6 +15,7 @@ mod duplicated_type;
 mod generics_const;
 mod generics_default;
 mod generics_lifetime;
+mod generics_multi_traits;
 mod generics_no_trait;
 mod generics_ok;
 mod generics_where_clause;
