02a524f9462131c1a5c25005124201d4d4b32378
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-25 17:09:57 -0500

    x

--- smt/testing/tests/model/parser/adt_struct_ok.rs
+++ smt/testing/tests/model/parser/adt_struct_ok.rs
@@ -1,14 +1,16 @@
 use rusmart_smt_remark::smt_type;
-use rusmart_smt_stdlib::dt::Boolean;
+use rusmart_smt_stdlib::dt::{Boolean, Cloak, Map, Seq, Set, SMT};
 
 #[smt_type]
 struct S1 {
-    f: Boolean,
+    f1: Boolean,
 }
 
 #[smt_type]
-enum E1 {
-    V1,
-    V2(S1),
-    V3 { f1: S1 },
+struct S2<T1: SMT, T2: SMT, T3: SMT> {
+    f1: S1,
+    f2: Cloak<T1>,
+    f3: Seq<T2>,
+    f4: Set<T3>,
+    f5: Map<T2, T3>,
 }
