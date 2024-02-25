02cb771fd9fe603404cfdec3ec403c28269ee9f8
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-02-25 17:47:35 -0500

    x

--- smt/testing/tests/model/parser/adt_struct_ok.rs
+++ smt/testing/tests/model/parser/adt_struct_ok.rs
@@ -2,12 +2,24 @@ use rusmart_smt_remark::smt_type;
 use rusmart_smt_stdlib::dt::{Boolean, Cloak, Map, Seq, Set, SMT};
 
 #[smt_type]
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Default)]
 struct S1 {
     f1: Boolean,
 }
 
+impl SMT for S1 {}
+
 #[smt_type]
-struct S2<T1: SMT, T2: SMT, T3: SMT> {
+struct S2 {
+    f1: S1,
+    f2: Cloak<S1>,
+    f3: Seq<S1>,
+    f4: Set<S1>,
+    f5: Map<S1, S1>,
+}
+
+#[smt_type]
+struct S3<T1: SMT, T2: SMT, T3: SMT> {
     f1: S1,
     f2: Cloak<T1>,
     f3: Seq<T2>,
