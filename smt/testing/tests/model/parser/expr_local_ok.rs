6927bf88cb107b14a460fade0d7470d027229e27
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-03-13 16:43:22 -0400

    x

---
+++ smt/testing/tests/model/parser/expr_local_ok.rs
@@ -0,0 +1,20 @@
+use rusmart_smt_remark::smt_impl;
+use rusmart_smt_stdlib::dt::{Boolean, Integer};
+
+#[smt_impl]
+fn f1() -> Boolean {
+    let x = Boolean::from(false);
+    x
+}
+
+#[smt_impl]
+fn f2() -> (Boolean, Integer) {
+    let x = (Boolean::from(false), Integer::from(0));
+    x
+}
+
+#[smt_impl]
+fn f3() -> (Boolean, Integer) {
+    let (x, y) = (Boolean::from(false), Integer::from(0));
+    (x, y)
+}
