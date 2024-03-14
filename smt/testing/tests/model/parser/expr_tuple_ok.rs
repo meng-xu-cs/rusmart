0cd1199fa02c1ff4d39f78aba87c66fe1e72fdd9
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-03-14 09:18:56 -0400

    x

---
+++ smt/testing/tests/model/parser/expr_tuple_ok.rs
@@ -0,0 +1,13 @@
+use rusmart_smt_remark::smt_impl;
+use rusmart_smt_stdlib::dt::{Boolean, Integer, SMT};
+
+#[smt_impl]
+fn f1() -> (Boolean, Integer) {
+    (Boolean::from(false), Integer::from(0))
+}
+
+#[smt_impl]
+fn f2<K: SMT, V: SMT>(k: K, v: V) -> (K, V) {
+    (k, v)
+}
+
