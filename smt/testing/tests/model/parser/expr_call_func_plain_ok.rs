9af466227fb4299b2aace36bf574450b6504d3d8
Meng Xu <meng.xu.cs@uwaterloo.ca>
2024-03-31 13:56:32 -0300

    x

---
+++ smt/testing/tests/model/parser/expr_call_func_plain_ok.rs
@@ -0,0 +1,50 @@
+use rusmart_smt_remark::{smt_impl, smt_type};
+use rusmart_smt_stdlib::dt::{Boolean, SMT};
+
+#[smt_impl]
+fn foo() -> Boolean {
+    Boolean::from(false)
+}
+
+#[smt_impl]
+fn bar() -> Boolean {
+    foo()
+}
+
+#[smt_type]
+struct S(Boolean);
+
+#[smt_impl]
+fn wrap_t_generic<T: SMT>(t: T) -> T {
+    t
+}
+
+#[smt_impl]
+fn call_wrap_t_sys_ty_no_arg() -> Boolean {
+    wrap_t_generic(Boolean::from(false))
+}
+
+#[smt_impl]
+fn call_wrap_t_sys_ty_with_arg() -> Boolean {
+    wrap_t_generic::<Boolean>(Boolean::from(false))
+}
+
+#[smt_impl]
+fn call_wrap_t_usr_ty_no_arg() -> S {
+    wrap_t_generic(S(Boolean::from(false)))
+}
+
+#[smt_impl]
+fn call_wrap_t_usr_ty_with_arg() -> S {
+    wrap_t_generic::<S>(S(Boolean::from(false)))
+}
+
+#[smt_impl]
+fn call_wrap_t_param_ty_no_arg<X: SMT>(x: X) -> X {
+    wrap_t_generic(x)
+}
+
+#[smt_impl]
+fn call_wrap_t_param_ty_with_arg<X: SMT>(x: X) -> X {
+    wrap_t_generic::<X>(x)
+}
